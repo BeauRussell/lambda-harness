0691722351af4e19a73fdca1ef085f1ca5bea2bd
Beau Russell <yourknightmares@gmail.com>
2026-01-14 09:20:16 -0600

    Switch to bubbletea

--- cmd/lth/main.go
+++ cmd/lth/main.go
@@ -6,22 +6,84 @@ import (
 	"os"
 
 	embed "github.com/BeauRussell/lambda-harness/internal/embedded"
-	"github.com/spf13/cobra"
+	tea "github.com/charmbracelet/bubbletea"
 )
 
-func runTest(cmd *cobra.Command, args []string) error {
-	path := "."
-	if len(args) > 0 {
-		path = args[0]
+type CLIModel struct {
+	NodeVersions []string
+	cursor int
+	selected map[int]struct{}
+}
+
+func initModel() CLIModel {
+	return CLIModel{
+		NodeVersions: []string{"20.X","22.X","24.X"},
+		selected: make(map[int]struct{}),
+	}
+}
+
+func (m CLIModel) Init() tea.Cmd {
+	return nil
+}
+
+func (m CLIModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+
+		case "up":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+
+		case "down":
+			if m.cursor < len(m.NodeVersions)-1 {
+				m.cursor++
+			}
+
+		case " ", "x":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+
+		case "enter":
+			runTest()
+
+		}
 	}
 
-	nodeVersions, _ := cmd.Flags().GetStringSlice("node")
-	matrix, _ := cmd.Flags().GetBool("matrix")
+	return m, nil
+}
 
-	fmt.Fprintf(cmd.OutOrStdout(), "Testing Lambda at: %s\n", path)
-	fmt.Fprintf(cmd.OutOrStdout(), "Node versions: %v\n", nodeVersions)
-	fmt.Fprintf(cmd.OutOrStdout(), "Matrix mode: %v\n", matrix)
+func (m CLIModel) View() string {
+	s := "What Node Versions would you like to test?\n\n"
 
+	for i, choice := range m.NodeVersions {
+		cursor := " "
+		if m.cursor == i {
+			cursor = ">"
+		}
+
+		checked := " "
+		if _, ok := m.selected[i]; ok {
+			checked = "x"
+		}
+
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	}
+
+	s += "\nPress q to quit.\n"
+
+	return s
+}
+
+func runTest() error {
 	result, err := embed.RunAnalyzer("/home/yourknightmares/repos/lth/lambdas/simple-cjs")
 	if err != nil {
 		log.Printf("Failed to run anaylzer: %v", err)
@@ -34,21 +96,9 @@ func runTest(cmd *cobra.Command, args []string) error {
 }
 
 func main() {
-	rootCmd := &cobra.Command{
-		Use:   "lth",
-		Short: "Lambda Test Harness",
-	}
-
-	testCmd := &cobra.Command{
-		Use:   "test [path]",
-		Short: "Test a Lambda",
-		Args:  cobra.MaximumNArgs(1),
-		RunE:  runTest,
-	}
-
-	rootCmd.AddCommand(testCmd)
-
-	if err := rootCmd.Execute(); err != nil {
+	p := tea.NewProgram(initModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("There's been an error: %v", err)
 		os.Exit(1)
 	}
 }
